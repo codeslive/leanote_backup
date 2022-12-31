05c87e6f04314aa2032f51c573a9566bf1f47b25
codesLive <2783779285@qq.com>
2023-01-01 03:13:52 +0800

    添加备份列表
    Signed-off-by: codesLive <2783779285@qq.com>

---
+++ js/download.js
@@ -0,0 +1,11 @@
+const downloadUrl = [
+  [
+    { id: 0, year: 2022 },
+    { id: 1, date: "2022-12", year: 2022, mounth: 12 },
+  ],
+  [
+    { id: 0, year: 2023 },
+    { id: 1, date: "2023-01", year: 2023, mounth: 01 },
+  ],
+]
+
